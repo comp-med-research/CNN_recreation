4ce5aba50c213ba9c89d17cbee47572d9ddcb1ac
comp-med-research <halimatafolabi@gmail.com>
2025-01-09 14:31:13 +0000

    first complete draft of model script

--- scripts/model.py
+++ scripts/model.py
@@ -2,63 +2,82 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CNN1:
+class CNN1(nn.Module):
     def __init__(self):
         super(CNN1,self).__init__()
-        self.input = nn.Embed(224,224,3)
-        self.cn1 = nn.Conv3d(3,3,16)
-        self.cn2 = nn.Conv3d(3,3,64)
-        self.cn3 = nn.Conv3d(3,3,256)
+        self.cn1 = nn.Conv3d(3,16,kernel_size=(1, 3, 3))
+        self.cn2 = nn.Conv3d(16,16,kernel_size=(1, 3, 3))
+        self.cn3 = nn.Conv3d(16,64,kernel_size=(1, 3, 3))
+        self.cn4 = nn.Conv3d(64,64,kernel_size=(1, 3, 3))
+        self.cn5 = nn.Conv3d(64,256,kernel_size=(1, 3, 3))
         self.relu = nn.ReLU()
-        self.maxpool = F.max_pool3d(2,2)
-        self.bn = nn.BatchNorm3d(128)
+        self.bn = nn.BatchNorm3d(256)
         self.dropout = nn.Dropout(p=0.2)
         self.fc = nn.Linear(160000,128)
         
 
     def forward(self, x):
+        # Embed the pixel values
+        x = x.float() 
         x = self.cn1(x)
-        x = self.cn1(x)
-        x = self.maxpool(x)
+        x = self.relu(x)
         x = self.cn2(x)
-        x = self.cn2(x)
-        x = self.maxpool(x)
+        x = self.relu(x)
+        x = F.max_pool3d(x,kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        x = self.cn3(x)
+        x = self.relu(x)
+        x = self.cn4(x)
+        x = self.relu(x)
+        x = F.max_pool3d(x,kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        x = self.cn5(x)
+        x = self.relu(x)
+        x = F.max_pool3d(x,kernel_size=(1, 2, 2), stride=(1, 2, 2))
         x = self.bn(x)
         x = self.dropout(x)
-        x = torch.flatten(x)
+        x = torch.flatten(x, start_dim=1)
         x = self.fc(x)
         
         return x
     
-class CNN2:
+class CNN2(nn.Module):
     def __init__(self):
         super(CNN2,self).__init__()
-        self.embed = nn.Embedding()
-        self.cn1 = nn.Conv3d(5,5,32)
-        self.cn2 = nn.Conv3d(5,5,128)
-        self.cn3 = nn.Conv3d(5,5,512)
+        self.cn1 = nn.Conv3d(3,32,kernel_size=(1, 5, 5))
+        self.cn2 = nn.Conv3d(32,32,kernel_size=(1,5, 5))
+        self.cn3 = nn.Conv3d(32,128,kernel_size=(1,5, 5))
+        self.cn4 = nn.Conv3d(128,128,kernel_size=(1, 5, 5))
+        self.cn5 = nn.Conv3d(128,512,kernel_size=(1, 5, 5))
         self.relu = nn.ReLU()
-        self.maxpool = F.max_pool3d(2,2)
-        self.bn = nn.BatchNorm3d(128)
+        self.bn = nn.BatchNorm3d(512)
         self.dropout = nn.Dropout(p=0.2)
         self.fc = nn.Linear(270848,128)
         
-    
+
     def forward(self, x):
+        # Embed the pixel values
+        x = x.float() 
         x = self.cn1(x)
-        x = self.cn1(x)
-        x = self.maxpool(x)
+        x = self.relu(x)
         x = self.cn2(x)
-        x = self.cn2(x)
-        x = self.maxpool(x)
+        x = self.relu(x)
+        x = F.max_pool3d(x,kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        x = self.cn3(x)
+        x = self.relu(x)
+        x = self.cn4(x)
+        x = self.relu(x)
+        x = F.max_pool3d(x,kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        x = self.cn5(x)
+        x = self.relu(x)
+        x = F.max_pool3d(x,kernel_size=(1, 2, 2), stride=(1, 2, 2))
         x = self.bn(x)
         x = self.dropout(x)
-        x = torch.flatten(x)
+        x = torch.flatten(x, start_dim=1)
         x = self.fc(x)
+        
         return x
 
 
-class TwoTowerCNN:
+class TwoTowerCNN(nn.Module):
     def __init__(self, CNN1, CNN2):
         super(TwoTowerCNN,self).__init__()
         self.CNN1 = CNN1
@@ -69,7 +88,7 @@ class TwoTowerCNN:
     def forward(self, x):
         a = self.CNN1(x)
         b = self.CNN2(x)
-        x = torch.cat((a, b), dim=0) 
+        x = torch.cat((a, b), dim=1) 
         x = self.fc(x)
         # classification layer 
         x = F.softmax(x, dim=1)
